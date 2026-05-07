585050ffdce28a4c590a077dc496ebc7a1965ecd
Gabriel Silva <killuado@awadev.com>
2026-05-07 00:54:16 -0300

    Early Access Fix + Select All Songs

--- Scripts/AutoLoad/MusicPlayer.gd
+++ Scripts/AutoLoad/MusicPlayer.gd
@@ -6,13 +6,13 @@ func _ready() -> void:
 	finished.connect(_finished)
 
 func _finished():
-	print("oi")
+	print("song finished")
 	if last_button:
 		last_button.texture_normal = preload("uid://4av6wmow3713")
 		last_button = null
 
 func load_song(path, button):
-	print(last_button)
+	#print(last_button)
 	if last_button:
 		if button == last_button:
 			last_button.texture_normal = preload("uid://4av6wmow3713")
